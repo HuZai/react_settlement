9935382d1975b911884f9335a13cf1d334c5a29b
HuZai <zhenghu1989415@163.com>
2017-08-24 10:57:35 +0800

    增加 项目访问路径basename处理

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import {render} from 'react-dom';
 import {Router,Route,browserHistory} from 'react-router'
+import useBasename from 'history/lib/useBasename'
 import App from './components/settlement';
 import Header from './components/Header';
 import SettlementTicketList from './components/settlement/ticket/SettlementTicketList';
@@ -12,13 +13,16 @@ import SettlementDeliveryMain from './components/settlement/delivery/SettlementD
 import SettlementProList from './components/settlement/SettlementProList';
 import SettlementReceiptMain from './components/settlement/receipt/SettlementReceiptMain';
 import InvalideProList from './components/settlement/InvalideProList';
+// 此处用于添加根路径
+const historyw = useBasename(() => browserHistory)({ basename: `/react_settlement1` });
+
 render(
-  <Router history={browserHistory}>
-    <Route path="/*.html(:cart)" component={App} />
-    <Route path="/SettlementTicketListForReact(:cart)" component={SettlementTicketList}/>
-    <Route path="/deliveryForReact(:cart)" component={SettlementDeliveryMain}/>
-    <Route path="/SettlementProListForReact(:cart)" component={SettlementProList}/>
-    <Route path="/receiptForReact(:cart)" component={SettlementReceiptMain}/>
+  <Router history={historyw}>
+    <Route path="/index.html" component={App} />
+        <Route path="SettlementTicketListForReact(:cart)" component={SettlementTicketList}/>
+        <Route path="deliveryForReact(:cart)" component={SettlementDeliveryMain}/>
+        <Route path="SettlementProListForReact(:cart)" component={SettlementProList}/>
+        <Route path="receiptForReact(:cart)" component={SettlementReceiptMain}/>
   </Router>
   , document.getElementById('main-page-view')
 );
