f14d23fdd56129b35416aeba31f7d20307bb1c09
HuZai <zhenghu1989415@163.com>
2019-04-26 14:58:37 +0800

    Update settementAction.js

--- src/actions/settementAction.js
+++ src/actions/settementAction.js
@@ -3,8 +3,6 @@
  */
 var SettementData = {
   getData: function (upParams,callback) {
-    //http://las.secoo.com/api/cart/confirm?cart={"cartType":"","aid":"5","ticketParam":{},"customsParam":{},"deliverTypeParam":[]}
-    //http://las.secoo.com/api/cart/confirm?cart={"cartType":"","shippingParam":{"shippingId":"421206"},"aid":"1","ticketParam":{"ticketId":"47534578"},"customsParam":{},"invoiceParam":{"invoiceType":"-1","invoiceTitle":" "},"deliverTypeParam":[]}&c_upk=c33b86724be5464090d1eabcec07cf8b|14002435405|45e71fd8d4e64cb7ac97294c771c40af|A8F8214D18173560995D1FEA8A6D3490&c_platform-type=2&c_platform=2
     var headerJson={
       'c_upk':getCookie('Sid'),
       'c_app_ver':'5.6.0',
@@ -13,7 +11,7 @@ var SettementData = {
     }
     $.extend(headerJson,{cart: decodeURIComponent(upParams)});
     $.ajax({
-      url: "http://las.secoo.com/api/cart/confirm",              //请求地址
+      url: "/api/cart/confirm",              //请求地址
       type: "get",                       //请求方式
       data: headerJson,        //请求参数
       dataType: "jsonp",
@@ -37,7 +35,7 @@ var SettementData = {
    ;
     $.extend(headerJson,{cart: decodeURIComponent(upParams)});
     $.ajax({
-      url: "http://las.secoo.com/api/cart/submit",//请求地址
+      url: "/api/cart/submit",//请求地址
       type: "get", //请求方式
       data: headerJson,//请求参数
       dataType: "jsonp",
